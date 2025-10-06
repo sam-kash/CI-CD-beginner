3513708b964b1c497f68058c9f7b9e6cb045bc18
sam-kash <kashyapsankalpa@gmail.com>
2025-10-06 14:55:26 +0530

    change

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
    return "Hello World!"
 
+@app.route("/welcome")
+def welcome():
+   return "welcome to github actions"
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
